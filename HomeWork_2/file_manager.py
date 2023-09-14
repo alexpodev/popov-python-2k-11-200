59c91181eb67cd550dff6c5f5b1b545517299b6a
Alexey Popov <alexey.popov.webdev@outlook.com>
2023-09-14 14:22:40 +0300

    add ls operation

--- HomeWork_2/file_manager.py
+++ HomeWork_2/file_manager.py
@@ -2,13 +2,18 @@ from pathlib import Path
 import os
 
 current_dir = Path('/')
-available_operations = ['pwd']
+available_operations = ['help', 'pwd', 'ls', 'cd', 'touch', 'cat', 'rm']
 
 while True:
     operation = input(f'{Path.absolute(current_dir)}$ ')
     if any(map(operation.__contains__, available_operations)):
         if operation == 'pwd':
             print(f"\n{Path.absolute(current_dir)}\n")
+        elif operation == 'ls':
+            try:
+                print(f"\n{os.listdir(current_dir)}\n")
+            except FileNotFoundError:
+                print('\nNo such file or directory\n')
             
     else:
         print(f'\nInvalid operation {operation}\n')
