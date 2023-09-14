ac230188a9503dc2cffcd915e473170ebd43e87d
Alexey Popov <alexey.popov.webdev@outlook.com>
2023-09-14 14:26:03 +0300

    add help operation

--- HomeWork_2/file_manager.py
+++ HomeWork_2/file_manager.py
@@ -7,18 +7,24 @@ available_operations = ['help', 'pwd', 'ls', 'cd', 'touch', 'cat', 'rm']
 while True:
     operation = input(f'{Path.absolute(current_dir)}$ ')
     if any(map(operation.__contains__, available_operations)):
-        if operation == 'pwd':
+        if operation == 'help':
+            print('----AVAILABLE OPERATIONS---- \n\npwd - print the path of the current working directory \nls - list the files in the current directory \ncd (dir_name) - change the current working directory \ntouch (file_name) - creating a blanck new file \nrm (file_name) - remove the file \ncat (file_name) - print the file content\n')
+            
+        elif operation == 'pwd':
             print(f"\n{Path.absolute(current_dir)}\n")
+            
         elif operation == 'ls':
             try:
                 print(f"\n{os.listdir(current_dir)}\n")
             except FileNotFoundError:
                 print('\nNo such file or directory\n')
+                
         elif operation.startswith('cd '):
             if operation[3:] == '..':
                 current_dir = current_dir.parent
             else:
                 current_dir = current_dir / operation[3:]
+                
         elif operation.startswith('touch '):
             file_name = operation[6:]
             with open(current_dir / file_name, 'w') as f:
