25fc5374a1885e92ef62c2969b6ce31b670c5836
Alexey Popov <alexey.popov.webdev@outlook.com>
2023-09-26 19:37:00 +0300

    resolve reviewed problems

--- HomeWork_3/find_max.py
+++ HomeWork_3/find_max.py
@@ -1,14 +1,13 @@
 from func_details_decorator import func_details_decorator
 
+
 @func_details_decorator
 def find_max(*args):
-    try:
-        max_val = 0
-        for i in args:
-            if i > max_val:
-                max_val = i
-        return max_val 
-    except TypeError as e:
-        return e
-    
+    max_val = 0
+    for i in args:
+        if i > max_val:
+            max_val = i
+    return max_val
+
+
 find_max(1, 2, 3, 4, 5, 6)
