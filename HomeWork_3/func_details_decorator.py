f4fa0c40df9c817fd3b0119e46088c3c8c8ba31a
Alexey Popov <alexey.popov.webdev@outlook.com>
2023-09-26 19:38:35 +0300

    resolve reviewed problems 2

--- HomeWork_3/func_details_decorator.py
+++ HomeWork_3/func_details_decorator.py
@@ -1,8 +1,8 @@
 def func_details_decorator(func):
     func_name = func.__name__
-    
+
     def wrapper(*args, **kwargs):
         print(f'{func_name}-INPUT: args = {args}, kwargs = {kwargs}')
         print(f'{func_name}-OUTPUT: {func(*args, **kwargs)}')
+        return func(*args, **kwargs)
     return wrapper
-        
