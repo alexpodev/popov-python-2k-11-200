46da849f8cb0f76c1c0c437d6e8ff4346bb572cb
Alexey Popov <alexey.popov.webdev@outlook.com>
2023-10-23 17:14:12 +0300

    add image_downloader.py

---
+++ HomeWork_8/image_downloader.py
@@ -0,0 +1,54 @@
+import os
+import requests
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import threading
+from url_normalaizer import normalize_image_url
+
+
+def download_image(image_url, save_path):
+    try:
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+    except Exception as e:
+        print(f"Ошибка при скачивании изображения {image_url}: {str(e)}")
+
+
+def download_images_from_page(page_url, save_folder):
+    response = requests.get(page_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        current_domain = urlparse(page_url).scheme + "://" + urlparse(page_url).netloc
+
+        img_tags = soup.find_all('img')
+        threads = []
+
+        for img_tag in img_tags:
+            img_url = img_tag.get('src')
+            if img_url:
+                img_url = normalize_image_url(img_url, current_domain)
+                img_filename = os.path.basename(urlparse(img_url).path)
+                save_path = os.path.join(save_folder, img_filename)
+
+            
+                thread = threading.Thread(target=download_image, args=(img_url, save_path))
+                thread.start()
+                threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+def main():
+    page_url = input("Введите адрес страницы: ")
+    save_folder = input("Введите папку для сохранения изображений: ")
+
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    download_images_from_page(page_url, save_folder)
+
+
+if __name__ == '__main__':
+    main()
